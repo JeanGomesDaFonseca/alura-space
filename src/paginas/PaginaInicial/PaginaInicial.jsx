ce394c7244215d95646d90c558019663daadd859
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-12-13 20:50:22 -0300

    criando novos componets

--- src/paginas/PaginaInicial/PaginaInicial.jsx
+++ src/paginas/PaginaInicial/PaginaInicial.jsx
@@ -1,11 +1,21 @@
+import Banner from "./banner.png"
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
 import Menu from "../../components/Menu/Menu";
+import styles from "./PaginaInicial.module.scss"
 
 export default function PaginaInicial() {
   return (
     <>
       <Cabecalho />
+      <main>
+        <section className={styles.principal}>
       <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={Banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
