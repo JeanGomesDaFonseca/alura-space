7df13b05d6b463c5fea9612f9507801f54d2c20b
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-12-19 21:32:28 -0300

    feito mais algumas partes. Descobrir o pq as imagens não estão renderizando

--- src/paginas/PaginaInicial/PaginaInicial.jsx
+++ src/paginas/PaginaInicial/PaginaInicial.jsx
@@ -1,7 +1,9 @@
-import Banner from "./banner.png"
+import Banner from "./banner.png";
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
 import Menu from "../../components/Menu/Menu";
-import styles from "./PaginaInicial.module.scss"
+import styles from "./PaginaInicial.module.scss";
+import Rodape from "../../components/Rodape/Rodape";
+import Galeria from "../../components/Galeria/Galeria";
 
 export default function PaginaInicial() {
   return (
@@ -9,13 +11,17 @@ export default function PaginaInicial() {
       <Cabecalho />
       <main>
         <section className={styles.principal}>
-      <Menu />
+          <Menu />
           <div className={styles.principal__imagem}>
             <h1>A galeria mais completa do espaço</h1>
             <img src={Banner} alt="A imagem da terra vista do espaço" />
           </div>
         </section>
       </main>
+      <div className={styles.galeria}>
+        <Galeria />
+      </div>
+      <Rodape />
     </>
   );
 }
