2af2b4aa4bd2ef49b4bc2e8ba0f12f49e8060718
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2022-12-28 10:50:54 -0300

    adicionando funcionalidade para os botões de filtros

--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Tags from "../Tags/Tags";
 import styles from "./Galeria.module.scss";
 import fotos from "./fotos.json";
@@ -6,12 +6,23 @@ import fotos from "./fotos.json";
 import Cards from "./Cards/Cards";
 
 export default function Galeria() {
-  console.log("fotos teste", fotos);
+
+  const [itens, setItens] = useState(fotos);
+  const tags = [...new Set(fotos.map((valor) => valor.tag))]
+
+  const filtraFotos = (tag) => {
+    const novasFotos = fotos.filter((foto) =>{
+      return foto.tag === tag;
+    })
+
+    setItens(novasFotos);
+  }
+
   return (
     <section className={styles.galeria}>
       <h2>Navegue pela Galeria</h2>
-      <Tags />
-      <Cards itens={fotos} styles={styles} />
+      <Tags tags={tags} filtraFotos={filtraFotos} setItens={setItens}/>
+      <Cards itens={itens} styles={styles} />
     </section>
   );
 }
